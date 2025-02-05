ccae51f1f38b15a83cee6a32c7f72302eca28cad
2382034 <2382034@unai.edu>
2025-02-05 11:17:38 +0700

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <table>
+        <tr>
+          <th>NIM</th>
+          <th>Nama</th>
+          <th>Jurusan</th>
+          <th>Email</th>
+          <th>Hobby</th>
+        </tr>
+        <tr>
+          <td>2382034</td>
+          <td>Bryan S Hursina</td>
+          <td>Sistem Informasi</td>
+          <td>bryanhursina091@gmail.com</td>
+          <td>Coding</td>
+        </tr>
+      </table>
+    </>
+  )
+}
+
+export default App
